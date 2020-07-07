39a8762f7daae9a62f476201b5e30250a40cc5eb
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-07-07 22:40:08 +0530

    Add Header Image And Fix Nav responsive

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,7 +5,7 @@ mix.js('src/js/script.js', 'dist/js/')
     .options({
         processCssUrls: false
     })
-    .copy('src/assets/**/*', 'dist/assets', false)
+    .copy('src/image/**/*', 'dist/image', false)
     .copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'dist/js', false)
     .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'dist/webfonts')
     .disableNotifications();
